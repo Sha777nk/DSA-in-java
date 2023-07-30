70c1b2bab8bb90c796e71aeea36c0009230295be
shashank <shashankchandanagiri@gmail.com>
2023-07-30 10:14:40 +0530

    Given an array arr[] of positive integers of size N. Reverse every sub-array group of size K

---
+++ arr_7.java
@@ -0,0 +1,27 @@
+public class arr_7 {
+    public static void reverseInGroup(int arr[],int n,int k){
+       
+        for(int i=0; i<n;i+=k){
+            int left = i;
+            int right = Math.min(i + k-1, n-1);
+
+            // Reverse the sub-array group
+            while (left < right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int []arr ={1,2,3,4,5};
+        int n=arr.length;
+        int k=3;
+        reverseInGroup(arr, n, k);
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
