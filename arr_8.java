a9a4387bfb56d8cc592b69493b24c5d499397f70
shashank <shashankchandanagiri@gmail.com>
2024-03-22 19:48:28 +0530

    Given an unsorted array arr[] of size N, rotate it by D elements in the counter-clockwise direction.

--- arr_8.java
+++ arr_8.java
@@ -1,24 +1,24 @@
 // Given an unsorted array arr[] of size N, rotate it by D elements in the counter-clockwise direction. 
 public class arr_8 {
-    public static void rotatearr(int arr[],int d,int n){
-        int[] temp =new int[d];
-        for(int j=0;j<d;j++){
+    public static void rotatearr(int arr[],int k,int n){
+        int[] temp =new int[k];
+        for(int j=0;j<k;j++){
             temp[j]=arr[j];
         }
-        for(int i=d;i<n;i++){
-            arr[i-d]=arr[i];
+        for(int i=k;i<n;i++){
+            arr[i-k]=arr[i];
         }
-        for (int i = n - d, j = 0; i < n; i++, j++) {
+        for (int i = n - k, j = 0; i < n; i++, j++) {
             arr[i] = temp[j];
         }
     }
     public static void main(String[] args) {
-        int []arr ={1,2,3,4,5,6};
+        int []arr ={1,2,3,4,5,6,7};
         int n=arr.length;
-        int d=1;
-        rotatearr(arr, d, n);
+        int k=3;
+        rotatearr(arr, k, n);
         for(int i=0;i<n;i++){
-            System.out.println(arr[i]);
+            System.out.print(arr[i]+" ");
         }
     }
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+ // for(int j=0;j<k;j++){
+        //     temp[j]=arr[j];
+        // }
